ad22be374eec62953b413b6a344c034ef80bc078
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-29 14:34:50 +0300

    Constructor Yapısı

---
+++ ConstructorYapisiniKafamizdaNetlestirelim/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Customer customer1 = new Customer
+{
+    Id = 1,
+    FirstName = "Kadir",
+    LastName = "Avşar"
+};
+
+Customer customer2 = new Customer(2, "Kadir", "Avşar","İstanbul");
+
+Console.WriteLine(customer2.City); 
+
+Customer customer3 = new Customer();
+
+class Customer
+{
+    public Customer()
+    {
+        Console.WriteLine();
+    }
+    public Customer(int id,string firstName,string lastName,string city)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+
+
+}
